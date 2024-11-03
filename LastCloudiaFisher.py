008f10b0357ce26ef968cc27e90ad02ebbc37bdb
Chris <shadowxcc@gmail.com>
2024-11-03 16:15:14 -0500

    Fixed comments

--- LastCloudiaFisher.py
+++ LastCloudiaFisher.py
@@ -1,28 +1,10 @@
-#https://humanbenchmark.com/tests/aim
+# https://www.lastcloudia.com/en/
 
 import time
 from PIL import ImageGrab
 import mouse
 import win32gui #pip install pywin32
-#dark blue color 29,67,97
-# != 34,108,167
 
-# X/Width = 780; Y/Height = 275
-
-# Monitor
-# width = 2560 
-# height = 1440 
-
-# for i in range(1):
-#     im = ImageGrab.grab()
-#     im.show()
-#     for :
-#         for :
-
-# global_x = 0
-# global_y = 0
-# global_w = 0
-# global_h = 0
 global_window_handle = 0
 
 def findWindow():
@@ -32,24 +14,13 @@ def findWindow():
 
 def callback(hwnd, extra):
     rect = win32gui.GetWindowRect(hwnd)
-    # x = rect[0]
-    # y = rect[1]
-    # w = rect[2] - x
-    # h = rect[3] - y
+
     if "Last Cloudia" == win32gui.GetWindowText(hwnd):
-        # global global_x
-        # global global_y
-        # global global_w
-        # global global_h
         global global_window_handle
 
-        # global_x = rect[0]
-        # global_y = rect[1]
-        # global_w = rect[2] - rect[0]
-        # global_h = rect[3] - rect[1]
         global_window_handle = hwnd
         print("Window %s:" % win32gui.GetWindowText(hwnd))
-        print(hwnd)
+        # print(hwnd)
         # print("\tLocation: (%d, %d)" % (global_x, global_y))
         # print("\t    Size: (%d, %d)" % (global_w, global_h))
         # print("Global X", global_x)
@@ -100,29 +71,18 @@ if __name__ == '__main__':
 
     runCount = 0
 
-    # global global_x
-    # global global_y
-    # global global_w
-    # global global_h
-
     while True:
         rect = win32gui.GetWindowRect(global_window_handle)
 
-        # global_x = rect[0]
-        # global_y = rect[1]
-        # global_w = rect[2] - rect[0]
-        # global_h = rect[3] - rect[1]
-
+        # THESE LINES DEFINE WHICH MAP THIS IS SET FOR
         # position = ladourBox(rect)
         position = ntBox(rect)
+        # THESE LINES DEFINE WHICH MAP THIS IS SET FOR
 
         # Scans left to right, top to bottom, like reading a book
         for i in range(1):
             im = ImageGrab.grab(bbox = position)
 
-            # im = ImageGrab.grab(bbox =(210, 425, 230, 475)) # This is the box for "Fishing Spot" next to Ladour
-            # im = ImageGrab.grab(bbox =(210, 530, 230, 550)) # This is the box for "Fishing Spot" next to Nameless Town
-            # im.show()
             # width, height = im.size
             for i, pixel in enumerate(im.getdata()):
                 if pixel == (255,255,255): #IF WHITE IS FOUND
@@ -130,28 +90,32 @@ if __name__ == '__main__':
                     # THE FOLLOWING 2 LINES ARE TEST CODE IF YOU WANT TO SEE THE PHOTO BEING CHECKED
                     # im.show()
                     # exit()
+                    # THE ABOVE 2 LINES ARE TEST CODE IF YOU WANT TO SEE THE PHOTO BEING CHECKED
 
-                    print('RunCount:', runCount, i, pixel)
-
-                    # mouse.click()
-                    # im.show()
-                    # a = a + 1
-                    # break
+                    print('\nRun:', runCount, 'Pixel Found at:', i)
 
                     # This is the code that automates clicking
                     # CLICK "REEL IN"
                     mouse.click()
+                    print(runCount, 'Clicked Reel In')
 
                     # WAIT FOR FISH TO BE REELED IN
+                    print(runCount, 'Waiting for fish to be reeled in')
                     time.sleep(3.6) #Big "Megrona Whale" take a long time to reel in, 3.5 seconds is less time than it takes
 
                     # CLICK "CLOSE" ON CAUGHT FISH PAGE
                     mouse.click()
+                    print(runCount, 'Clicked Close')
 
                     # Wait for 
+                    print(runCount, 'Waiting for screen change')
                     time.sleep(.5) #1.4 worked 100% over 100's (500+) of runs
 
                     #CLICK "CAST OUT"
                     mouse.click()
+                    print(runCount, 'Clicked Cast Out')
+
+
+
                     runCount = runCount + 1
-                    break
+                    break # Exit closest 'for' loop, return to looking for next caught fish
